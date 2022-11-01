c89235e9ee4c24cb3c96d5ac114eb4ed0ea6576b
larasodjati <108501073+larasodjati@users.noreply.github.com>
2022-11-01 11:32:50 -0700

    answers for lesson 3

---
+++ 02-express-tutorial/practiceExpress.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// for '/get request, it should return the index.html from the new-public folder
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './new-public/index.html'));
+})
+
+// for ‘/sample’ get request, it should just return the line “This is working"
+app.get('/sample', (req,res) => {
+    res.send('This is working');
+})
+
+//Configure using port 3000
+app.listen(3000, () => {
+    console.log('server is listening on port 3000...');
+})
