196987a18d9ab1ca28add20701b34e05dad28229
larasodjati <108501073+larasodjati@users.noreply.github.com>
2022-11-08 19:03:34 -0800

    answers for lesson 4

--- 02-express-tutorial/practiceExpress.js
+++ 02-express-tutorial/practiceExpress.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const consoleLog = require('./practice-middleware');
+
+app.use(consoleLog);
 
 // for '/get request, it should return the index.html from the new-public folder
 app.get('/', (req, res) => {
