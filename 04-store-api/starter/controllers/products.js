b20f27706fab90ce15526212ebfc4615fe1d5d7b
larasodjati <108501073+larasodjati@users.noreply.github.com>
2022-12-06 12:59:02 -0800

    answers for lesson week7

---
+++ 04-store-api/starter/controllers/products.js
@@ -0,0 +1,19 @@
+const Product = require('../models/product');
+
+const getAllProductsStatic = async (req, res) => {
+    //throw new Error('testing async errors')
+    const products = await Product.find({
+        name:'vase table'
+        
+    })
+    res.status(200).json({products, nbHits: products.length})
+}
+
+const getAllProducts = async (req, res) => {
+    res.status(200).json({msg: 'products route'})
+}
+
+module.exports = {
+    getAllProducts,
+    getAllProductsStatic,
+}
