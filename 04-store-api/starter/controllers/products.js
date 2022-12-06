4c5a278dd352ed0bb9095db1af6cde21fdc7cec8
larasodjati <108501073+larasodjati@users.noreply.github.com>
2022-12-06 15:00:06 -0800

    answers for lesson week7

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 const getAllProductsStatic = async (req, res) => {
     //throw new Error('testing async errors')
     const products = await Product.find({
-        name:'vase table'
+        name:'albany sectional',
         
     })
     res.status(200).json({products, nbHits: products.length});
@@ -11,7 +11,20 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
     //console.log(req.query);
-    const products = await Product.find(req.query);
+    const {featured, company, name} = req.query;
+    const queryObject = {};
+
+    if (featured){
+        queryObject.featured = featured ==='true'? true: false
+    }
+    if(company){
+        queryObject.company = company;
+    }
+    if (name){
+        queryObject.name = name;
+    }
+    console.log(queryObject);
+    const products = await Product.find(queryObject);
     res.status(200).json({products, nbHits: products.length});
 }
 
